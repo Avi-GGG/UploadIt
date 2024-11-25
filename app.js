3727d800e8e630e0b83f7d537b6366b42c618743
Avi-GG <avigtt15@gmail.com>
2024-11-25 20:31:24 +0530

    first commit

--- app.js
+++ app.js
@@ -31,6 +31,6 @@ app.get('/', (req, res) => {
     res.render('index');
 })
 
-app.listen(process.env.PORT || 3000, ()=> {
-    console.log('Server is running on port 3000');
+app.listen(process.env.PORT || 4000, ()=> {
+    console.log('Server is running on port 4000');
 })
