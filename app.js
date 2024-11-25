8e9bb6fd14a9abff5c3913945c434088d9dfc3cb
Avi-GG <avigtt15@gmail.com>
2024-11-25 16:43:38 +0530

    first commit

--- app.js
+++ app.js
@@ -6,19 +6,13 @@ const connectToDb = require('./config/db');
 connectToDb();
 const cookieParser = require('cookie-parser')
 const {router} = require('./routes/index.routes')
-const session = require('express-session');
 const path = require('path');
 const fileRouter = require('./routes/file.routes'); // Adjust the path as needed
 const uploadRouter = require('./routes/upload.routes');
 
 
 // Session middleware setup
-app.use(session({
-    secret: process.env.SESSION_KEY,  // Replace with a secure secret string
-    resave: false,
-    saveUninitialized: true,
-    cookie: { secure: false }  // Use true only for HTTPS
-}));
+
 const userRouter = require('./routes/user.routes');
 app.set('view engine', 'ejs');
 // app.set('views', path.resolve(__dirname, '../views'));
